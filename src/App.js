397241d5e9ea6ca756dde10f78ea2f9e502a1e49
Hunter Wallen <hunterwallen67@gmail.com>
2024-05-08 17:16:41 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState()
+  const [initialFetchComplete, setInitialFetchComplete] = useState(false)
+
+  const getUserFromDb = async () => {
+    try {
+      const data = await fetch(process.env.REACT_APP_BACKEND_URL)
+      const foundUser = await data.json()
+      setUser(foundUser)
+      setInitialFetchComplete(true)
+    } catch (err) {
+      console.log(err)
+      setInitialFetchComplete(true)
+    }
+  }
+
+  useEffect(() => {
+    getUserFromDb()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Basic Deployment Example Frontend</h1>
+      {
+        user && (
+          <>
+            <h3>Username: {user.username}</h3>
+            <h3>Email: {user.email}</h3>
+          </>
+        )
+      }
+      {
+        initialFetchComplete && !user && (
+          <h2>No User Found</h2>
+        )
+      }
     </div>
   );
 }
